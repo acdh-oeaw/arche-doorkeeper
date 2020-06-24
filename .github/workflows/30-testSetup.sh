808593291a6c2a29a32bd2724553019613ed5c43
Mateusz Żółtak <zozlak@zozlak.org>
2020-06-24 21:17:44 +0200

    CI works (hopefully)

--- .github/workflows/30-testSetup.sh
+++ .github/workflows/30-testSetup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo "DELETE FROM metadata WHERE property = 'https://vocabs.acdh.oeaw.ac.at/schema#vocabs' AND value ~ 'iso|oefos';" | psql
 
+rm -fR /home/www-data/vendor/acdh-oeaw/arche-doorkeeper
+ln -s /home/www-data/arche-doorkeeper /home/www-data/vendor/acdh-oeaw/arche-doorkeeper
+
 CMD=/home/www-data/vendor/zozlak/yaml-merge/bin/yaml-edit.php
 HD=/home/www-data
 rm -f /home/www-data/vendor/acdh-oeaw/arche-doorkeeper/config.yaml
